e33613319a93e50812c370fe65ff1b4535ecbcf7
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-09-25 15:34:57 +0900

    week05 v1.1

--- week05/main.go
+++ week05/main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("Input Score : ")
-	reader := bufio.NewReader(os.Stdin)
-	inputScoreString, err:= reader.ReadString('\n') // option 2
-	if err != nil{
-		log.Fatal(err)
-	}
-	inputScoreString = strings.TrimSpace((inputScoreString)) // remove space bar
-	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // casting
-	if err != nil{
-		log.Fatal(err)
-	}
-	var grade string
-	if inputScore >= 90 { 
-		grade = "A grade!"
-	} else {
-		grade = "under A grade~"
-	}
-	fmt.Println("you got " + grade)
+	dice := rand.Intn(6)+1
+	fmt.Println((dice))
 }
 	
