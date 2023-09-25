fadcafcaa7e28472d29c65db7d3f2e0eab277c90
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-09-25 15:20:03 +0900

    week05 v0.9

--- week05/main.go
+++ week05/main.go
@@ -5,17 +5,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("Input Score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err:= reader.ReadString('\n') // option 2
+	inputScoreString, err:= reader.ReadString('\n') // option 2
 	if err != nil{
 		log.Fatal(err)
 	}
-	fmt.Println(inputScore)
-	if inputScore >= 90{
+	inputScoreString = strings.TrimSpace((inputScoreString)) // remove space bar
+	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // casting
+	if err != nil{
+		log.Fatal(err)
+	}
+	if inputScore >= 90 { //mismatched types string and untyped int
 		grade := "A grade!"
 	} else {
 		grade := "BCDE grade~"
