2d81292d5ecb8212cf1c4de8f1b49e744d709416
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-09-25 15:45:16 +0900

    week05 v1.2

--- week05/main.go
+++ week05/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"time"
 )
 
 func main() {
-	dice := rand.Intn(6)+1
-	fmt.Println((dice))
+	rand.Seed(time.Now().Unix()) // get the current data and time as an integer
+	answer := rand.Intn(100) + 1// random integer number between (1 ~ 100)
+	fmt.Println("Guess Number Game~")
+	fmt.Println(answer)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Input guess nuber : ")
+	inputNumber, err := reader.ReadString('\n')
+	if err = nil {
+		log.Fatal(err)
+	}
+	
 }
 	
