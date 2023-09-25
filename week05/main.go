f346ed9b79fc7b6f1a5d20cd3763f1263d8c3eb2
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-09-25 15:07:23 +0900

    week05 v0.6

--- week05/main.go
+++ week05/main.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Print("Input Score : ")
 	reader := bufio.NewReader(os.Stdin)
 	inputScore, err:= reader.ReadString('\n') // option 2
-	log.Fatal(err)
+	if err != nil{
+		log.Fatal(err)
+	}
 	fmt.Println(inputScore)
 }
