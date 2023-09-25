38114f60d355c947cf138833be0132759342e270
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-09-25 14:25:52 +0900

    week05 v0.2

--- week05/main.go
+++ week05/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time 
-	now = time.Now()
-	year := now.Year()
-	var month string = now.Month().String()
-
-	fmt.Println(year, month, now.Hour(), now.Minute(), now.Second())
+	HotSpurs := "hm ? j madi?"
+	replacePlayer := strings.NewReplacer("?", "son")
+	player := replacePlayer.Replace(HotSpurs)
+	fmt.Println(player) 	
 }
