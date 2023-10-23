5a9d1986464dc2d6f399a8f3b94109fee99ec461
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-10-23 14:57:21 +0900

    week09 v1.8

--- week09/main.go
+++ week09/main.go
@@ -2,11 +2,20 @@ package main
 
 import "fmt"
 
+func swap(n1 *int, n2 *int){
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
+}
 
 func main(){
 	a := 10
-	// b := 20 // var b int = 20
+	b := 20 // var b int = 20
 	
-	c := &a
+	c := &a // var c *int = &a
 	fmt.Printf("%T\n", c)
+	fmt.Println(a, b)
+
+	swap(&a, &b) // pass by pointer
+	fmt.Println(a, b)
 }
