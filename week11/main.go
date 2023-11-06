ba4e386361da589d0355f4980f433c55985d1821
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-11-06 14:24:14 +0900

    week11 v2.4

--- week11/main.go
+++ week11/main.go
@@ -18,5 +18,14 @@ func main(){
 	fmt.Println(primes, primes[1])
 	
 	test := [5]bool{true, true, true}
-	fmt.Println(test[3])
+	fmt.Println(test[3]) //boolean 타입의 제로값, false
+
+	// fmt.Println(primes[4]) // invalid argument : index  4 out of bounds[0:3]
+
+	i := 0
+	for i< 4{ // panic: runtime error: index out of range [3] with length 3
+		fmt.Println(primes[i]) 
+		i++
+	}
+
 }
