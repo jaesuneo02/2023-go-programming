8425140bb4b61237f04ee8ce4d820ecb55f87e17
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-11-13 14:26:36 +0900

    week12 v3.0

--- week12/main.go
+++ week12/main.go
@@ -4,16 +4,18 @@ import "fmt"
 
 func main() {
 
-	// var s []int
-	// s = make([]int, 5) // make함수를 이용해 슬라이스 생성 후 메모리 할당, 제로 값 적용
-	
-	// s :=make([]int, 5)
-	s := []int{0,0,0,0,0} //  슬라이스 리터럴을 이용해 슬라이스 생성 및 메모리 할당, 초기화 진행
+	s := []int{0,0,0,0,0}
 	
 	s[4] = 99
 	s[2] = 91
 
 	for _, value := range s {
+		fmt.Println(value)
+	}
+
+	copyS := s[1:4]
+
+	for _, value := range copyS {
 		fmt.Println(value)
 	}
 }
