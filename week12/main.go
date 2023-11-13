8bca61206f362bbf2f68a776b0ff6cffb220e664
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-11-13 14:59:39 +0900

    week12 v3.3

--- week12/main.go
+++ week12/main.go
@@ -4,29 +4,15 @@ import "fmt"
 
 func main() {
 
-	s := []int{0,0,0,0,0}
-	
-	s[4] = 99
-	s[2] = 91
+	a := []string{"a", "b", "c", "d"}
 
-	for _, value := range s {
-		fmt.Println(value)
-	}
+	as := a[0:2]
+	as[1] = "z"
+	fmt.Println(a)
+	fmt.Println(as)
 
-	copyS := s[1:4]
-
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
-	test := [3] string{"inha","go","student"} // 배열 리터럴을 이용해서 test 배열 생성
-	// testS := test[0:4]                     //invalid argument: index 4 out of bounds [0:4]
-	testS:= test[:2] //testS := test[0:2]
-	testS2 := testS[1:]
-	testS2[0] = "python"
-	// fmt.Println(testS2[1])
-	
-	fmt.Println(testS2)
-	fmt.Println(testS, len(testS))
-	fmt.Println(test)
-
+	b := [4]int(4,3,2,1)
+	bs := b[1:3]
+	fmt.Println(bs) 
 }
+
