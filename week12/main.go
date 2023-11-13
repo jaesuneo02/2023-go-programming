f38dc0e62998af976583f4b83f7b22aa0d2dbf30
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-11-13 14:23:29 +0900

    week12 v2.8

--- week12/main.go
+++ week12/main.go
@@ -9,6 +9,10 @@ func main() {
 	
 	// s :=make([]int, 5)
 	s := []int{0,0,0,0,0} //  슬라이스 리터럴을 이용해 슬라이스 생성 및 메모리 할당, 초기화 진행
+	
+	s[4] = 99
+	s[2] = 91
+
 	for _, value := range s {
 		fmt.Println(value)
 	}
