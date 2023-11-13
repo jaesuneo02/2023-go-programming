451c87b48f26c2c2641b831e1dfde7254890780a
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-11-13 14:56:17 +0900

    week12 v3.2

--- week12/main.go
+++ week12/main.go
@@ -18,9 +18,15 @@ func main() {
 	for _, value := range copyS {
 		fmt.Println(value)
 	}
-
 	test := [3] string{"inha","go","student"} // 배열 리터럴을 이용해서 test 배열 생성
 	// testS := test[0:4]                     //invalid argument: index 4 out of bounds [0:4]
-	testS:= test[0:2]
+	testS:= test[:2] //testS := test[0:2]
+	testS2 := testS[1:]
+	testS2[0] = "python"
+	// fmt.Println(testS2[1])
+	
+	fmt.Println(testS2)
 	fmt.Println(testS, len(testS))
+	fmt.Println(test)
+
 }
