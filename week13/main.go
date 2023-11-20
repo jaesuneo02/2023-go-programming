a581b83ba6ff930c28ecbdbfccffd8d586bb02bd
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-11-20 14:09:29 +0900

    week13 v3.8

--- week13/main.go
+++ week13/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-
+ 
 	var a []string
+	var b []bool
 	a = make([]string, 4,5)
-	fmt.Printf("%#v\n",a)
+	fmt.Printf("%#v %#v\n",a , b)
 	fmt.Println(a,len(a), cap(a))
+	fmt.Println(b,len(b), cap(b))
+
 }
