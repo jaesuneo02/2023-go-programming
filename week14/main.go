05f0b3f225644ad272ad25ca0849aca106a94448
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-11-27 14:25:34 +0900

    week14 v4.2

--- week14/main.go
+++ week14/main.go
@@ -5,17 +5,23 @@ import "fmt"
 
 
 func main(){
-	var games map[int]string
-	games = make(map[int]string)
-
-	games[456] = "성기훈"
-	games[218] = "박해수"
-	games[067] = "강새벽"
-	games[001] = "오일남"
-	games[199] = "알리"
-	games[101] = "아이오아이"
+	games := map[int]string{
+		456 : "성기훈",
+		218 : "박해수",
+		67 : "강새벽",
+		1 : "오일남",
+		199 : "알리",
+		101: "아이오아이",
+	}
+name, ok := games[100]
+fmt.Println(name, ok)
 
 	for _, v := range games{
 		fmt.Println(v)
+	}
+	games[101] = "장덕수" // update
+	delete(games,199) // delete
+	for k, v := range games {
+		fmt.Println(k,v)
 	}
 }
