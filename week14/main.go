0c477fd0aacc23013f5ded5eb2cf54455729ec31
JaeSUn <67827517+wotjs020708@users.noreply.github.com>
2023-11-27 14:45:38 +0900

    week14 v4.4

--- week14/main.go
+++ week14/main.go
@@ -4,11 +4,23 @@ import "fmt"
 
 
 
+func status(name string){
+	balls := map[string]int{"성기훈" : 20, "오일남" : 0,}
+	// ball := balls[name]
+	// fmt.Println(ball)
+	ball, exists := balls[name]
+	if !exists{
+		fmt.Println(name, "님은 게임 참옂가 아닙니다.")
+	} else if ball < 1 {
+		fmt.Println(name, "님은", balls[name], "개로 탈락~ 탕탕")
+		} else {
+			fmt.Println(name, "님은 게임에서 승리하셨습니다.")
+	}
+}
+
 func main(){
-	var balls map[string]int
-	// balls := make(map[string]int)
-	fmt.Printf("%#v\n",balls)
-	balls["성기훈"] = 20
-	fmt.Println(balls["성기훈"])
-	fmt.Println(balls["오일남"])
+	status("오일남")
+	status("강철")
+	status("성기훈")
+	
 }
